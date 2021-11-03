4e03432d7897af36f35551ad90efe7c27cbedc00
Fionnuala Solomon <fisolomon@ichec.ie>
2021-11-03 14:22:26 +0000

    added comment

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highscore = 0;
 
+// changed ...
+
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
   // No input given by player
